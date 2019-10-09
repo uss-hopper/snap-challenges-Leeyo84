0010970b3849b9d9250427bef9f76e99b21e8a7b
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-09 09:56:17 -0600

    added a couple of functions to scrip.js

---
+++ snap-10-9/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Snap Challenge One</title>
+	<script src="script.js"></script>
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ snap-10-9/script.js
@@ -0,0 +1,11 @@
+function functionName(param) {
+	return param * param;
+}
+
+console.log(functionName(3,7))
+
+function concatstrings(firstName, lastName) {
+	return firstName + ' ' + lastName
+}
+
+	console.log(concatstrings('Leo', 'Guti'))
