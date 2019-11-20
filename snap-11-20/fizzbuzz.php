30d82a221ccf8aab94ffee21b6bc5f37b0474477
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-11-20 10:39:34 -0700

    w/ martys answer

--- snap-11-20/fizzbuzz.php
+++ snap-11-20/fizzbuzz.php
@@ -1,11 +1,20 @@
 <?php
 
+/** martys answer */
 function counts ($array) {
+
 	$newArray = [];
+
 	foreach($array as $element){
+		if(isset($newArray[$element])){
 		$newArray = [$element => $newArray[$element] + 1];
-	}
+		}else {
+			$newArray[$element] = 1;
+		};
+	};
 	return $newArray;
-}
+	};
+
+
 $origArray = [1, 1, 1, 2, 2, 3, 4, 5, 4];
 var_dump(counts($origArray));
