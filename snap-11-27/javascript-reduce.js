2e7dbdc68e0058a7df3954edb32299902dd0adef
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-11-27 08:17:50 -0700

    did this in less than 15 minutes... :)

---
+++ snap-11-27/javascript-reduce.js
@@ -0,0 +1,18 @@
+const nums = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+console.log(nums.reduce(reducer));
+
+
+console.log(nums.reduce(reducer, 5));
+
+
+const array = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
+
+const string = array.reduce(function(prevVal,currVal,idx){
+	return idx == 0 ? currVal : prevVal + ', ' + currVal;
+}, '');
+
+console.log(string);
